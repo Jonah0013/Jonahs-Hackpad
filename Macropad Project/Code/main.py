c13b03d79eb489d10928ca0ec10f0e4ee191c5fb
jonah <211564481+Jonah0013@users.noreply.github.com>
2025-12-06 11:37:41 +0000

    led holes in cover and updated led code
    led0 allways on green (power indicator
    led1 flashes every time a key is pressed (looks cool and if the key stops working it will help work out why

--- Macropad Project/Code/main.py
+++ Macropad Project/Code/main.py
@@ -1,35 +1,79 @@
-# You import all the IOs of your board
 import board
+import time
 
-# These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.modules.rgb import RGB
 
-# This is the main instance of your keyboard
 keyboard = KMKKeyboard()
 
-# Add the macro extension
+# --- MACROS ---
 macros = Macros()
 keyboard.modules.append(macros)
 
-# Define your pins here!
+# --- LED SETUP (SK6812MINI) ---
+rgb = RGB(
+    pixel_pin=board.SDA,   # Your GPIO6/SDA pin
+    num_pixels=2,          # You have 2 LEDs daisy-chained
+    val_limit=100,
+)
+keyboard.modules.append(rgb)
+
+# Turn on power LED (LED 0 = green)
+def set_power_led():
+    rgb.pixels[0] = (0, 255, 0)   # Green
+    rgb.show()
+
+set_power_led()
+
+# Flash LED 1 red
+def flash_key_led():
+    rgb.pixels[1] = (255, 0, 0)   # Red
+    rgb.show()
+    time.sleep(0.1)               # 100ms flash
+    rgb.pixels[1] = (0, 0, 0)     # Off
+    rgb.show()
+
+# --- KEY PINS ---
 PINS = [board.D3, board.D4, board.D2, board.D1]
 
-# Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
     pins=PINS,
     value_when_pressed=False,
 )
 
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+# --- KEYMAP ---
 keyboard.keymap = [
-    [KC.Macro(Press(KC.LCTRL), Tap(KC.C), Release(KC.LCTRL)), KC.Macro(Press(KC.LCTRL), Tap(KC.V), Release(KC.LCTRL)), KC.Macro(Press(KC.LCTRL), Tap(KC.Z), Release(KC.LCTRL)), KC.Macro(Press(KC.LCTRL), Tap(KC.S), Release(KC.LCTRL)),]
+    [
+        KC.Macro(Press(KC.LCTRL), Tap(KC.C), Release(KC.LCTRL)),
+        KC.Macro(Press(KC.LCTRL), Tap(KC.V), Release(KC.LCTRL)),
+        KC.Macro(Press(KC.LCTRL), Tap(KC.Z), Release(KC.LCTRL)),
+        KC.Macro(Press(KC.LCTRL), Tap(KC.S), Release(KC.LCTRL)),
+    ]
 ]
 
-# Start kmk!
+# --- Add keypress hook ---
+last_state = keyboard.matrix.get_state()
+
+def on_keypress():
+    flash_key_led()
+
+keyboard.before_matrix_scan = lambda: None
+
+def check_keys():
+    global last_state
+    new_state = keyboard.matrix.get_state()
+
+    if new_state != last_state:     # a key state changed
+        if True in new_state:       # a key is pressed
+            on_keypress()
+
+    last_state = new_state
+
+keyboard.before_matrix_scan = check_keys
+
+# --- START KMK ---
 if __name__ == '__main__':
     keyboard.go()
